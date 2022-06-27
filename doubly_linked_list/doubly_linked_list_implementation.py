005dd3925e949ba87066810b79cdd700b3a34eed
Dawana Baker <krunk_011@hotmail.com>
2022-06-26 23:35:05 -0500

    update doubly linked list example

--- doubly_linked_list/doubly_linked_list_implementation.py
+++ doubly_linked_list/doubly_linked_list_implementation.py
@@ -8,17 +8,20 @@ tinashePlaylist.add_to_head("Looking 4 It")
 tinashePlaylist.add_to_head("333")
 print(tinashePlaylist.stringify_list())
 
-tinashePlaylist.remove_tail()
-print(tinashePlaylist.stringify_list())
-
 tinashePlaylist.add_to_tail("Dreams Are Real")
 tinashePlaylist.add_to_tail("Black Water")
 tinashePlaylist.add_to_tail("Just A Taste")
 tinashePlaylist.add_to_head("Middle Of Nowhere")
 print(tinashePlaylist.stringify_list())
 
+tinashePlaylist.remove_tail()
 tinashePlaylist.remove_head()
 print(tinashePlaylist.stringify_list())
+
+tinashePlaylist.remove_by_value("Let Go")
+print(tinashePlaylist.stringify_list())
+
+
 
 
 
