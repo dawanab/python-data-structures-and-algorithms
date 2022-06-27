dbd333e8c3140ac84f12b3190ed285b738971479
dawanabaker <krunk_011@hotmail.com>
2022-06-27 17:58:59 -0500

    added two pointer techniques for linked list

---
+++ linked_list/two_pointer_ll_techniques/two_pointers_in_parallel.py
@@ -0,0 +1,105 @@
+# Create a method that returns the nth last element of a singly LL
+
+"""  
+The pseudocode for this method is:
+    nth last pointer = None
+    tail pointer - linked list head
+    count = 1
+    
+    while tail pointer exists:
+        move tail pointer forward
+        increment count
+    
+        if count >= n + 1:
+            if nth last pointer is None:
+                set nth last pointer to head
+            else:
+                move nth last pointer forward
+
+    return nth last pointer
+"""
+
+
+class Node:
+  def __init__(self, value, next_node = None, prev_node = None):
+    self.value = value
+    self.next_node = next_node
+    self.prev_node = prev_node
+    
+  def set_next_node(self, next_node):
+    self.next_node = next_node
+    
+  def get_next_node(self):
+    return self.next_node
+
+  def set_prev_node(self, prev_node):
+    self.prev_node = prev_node
+    
+  def get_prev_node(self):
+    return self.prev_node
+  
+  def get_value(self):
+    return self.value
+
+class LinkedList:
+
+    def __init__(self, value = None):
+        self.head_node = Node(value)
+
+    def get_head_node(self):
+        return self.head_node
+
+    def insert_beginning(self, new_value):
+        self.new_node = Node(new_value)
+        self.new_node.set_next_node(self.head_node)
+        self.head_node = self.new_node
+
+    def stringify_list(self):
+        string_list = ""
+        current_node = self.get_head_node()
+        while current_node:
+            if current_node.get_value() != None:
+                string_list += str(current_node.get_value()) + "\n"
+            current_node = current_node.get_next_node()
+        return string_list
+
+    def remove_node(self, value_to_remove):
+        current_node = self.head_node
+        if current_node.get_value() == value_to_remove:
+            self.head_node = current_node.get_next_node()
+        else:
+            while current_node:
+                next_node = current_node.get_next_node()
+                if next_node.get_value() == value_to_remove:
+                    current_node.set_next_node(next_node.get_next_node())
+                    current_node = None
+                else:
+                    current_node = next_node
+
+
+
+def nth_last_node(linked_list, n):
+  current = None
+  tail_seeker = linked_list.head_node
+  count = 1
+  while tail_seeker:
+    tail_seeker = tail_seeker.get_next_node()
+    count += 1
+    if count >= n + 1:
+      if current is None:
+        current = linked_list.head_node
+      else:
+        current = current.get_next_node()
+  return current
+
+def generate_test_linked_list():
+    linked_list = LinkedList()
+    for i in range(50, 0, -1):
+        linked_list.insert_beginning(i)
+    return linked_list
+
+# Use this to test your code:
+test_list = generate_test_linked_list()
+print(test_list.stringify_list())
+nth_last = nth_last_node(test_list, 4)
+print(nth_last.value)
